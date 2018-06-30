7ef767b9c9d697d51ec920ccbfce5b0dffd3fd9a
Deborah Seow <deborahseow9@gmail.com>
2018-06-30 13:16:17 -0500

    Fixed @user not going to show route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,31 +5,33 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
-    if @user.invalid?
-      render :new
+    @user = User.create(user_params)
+    if @user
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+      binding.pry
     else
-    @user.save
-    session[:user_id] = @user.id
-    binding.pry
-    redirect_to user_path(@user)
+      render :new
     end
   end
+  # def create
+  #   @user = User.new(user_params)
+  #   @user.save ? (session[:user_id] = @user.id; redirect_to user_path(@user)) : (render :new)
+  # end
 
   def show
+    @user = User.find_by(id: session[:user_id])
   end
 
   def edit
   end
 
   def destroy
-
     redirect_to '/'
   end
 
   private
-
   def user_params
-    params.require(:user).permit(:user_id, :email, :username, :password, :password_confirmation)
+    params.require(:user).permit(:email, :username, :password, :password_confirmation)
   end
 end
