511829698bc45282f2f369baa47ae1ed2dc918db
Deborah Seow <deborahseow9@gmail.com>
2018-06-30 21:53:47 -0500

    Changed from following users to following books

--- app/models/book.rb
+++ app/models/book.rb
@@ -7,6 +7,9 @@ class Book < ApplicationRecord
   has_many :user_books
   has_many :users, through: :user_books
 
+  has_many :follower_relationships, foreign_key: :following_id, class_name: 'FollowingFollower'
+  has_many :followers, through: :follower_relationships, source: :follower
+
   validates :title, presence: true
   validates :user_ids, presence: true
 end
